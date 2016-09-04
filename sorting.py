c3e4f0dcf46bf4e7422d79d444ed727a0d39910a
Whitney Belba <whitneybelba@gmail.com>
2016-09-04 17:36:57 +0000

    finished assessment

---
+++ sorting.py
@@ -0,0 +1,78 @@
+#Sorting
+
+def bubble_sort(lst):
+    """returns a sorted list using a optimized bubble sort algorithm
+    i.e. using a variable to track if there hasn't been a swap
+        >>> bubble_sort([3, 5, 7, 2, 4, 1])
+        [1, 2, 3, 4, 5, 7]
+    """
+
+    for i in range(len(lst) - 1):
+        made_swap = False
+        for j in range(len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                made_swap = True
+        if not made_swap:
+            break
+
+    return lst
+
+
+
+def merge_lists(list1, list2):
+    """Given two sorted lists of integers, returns a single sorted list containing all
+    integers in the input lists
+    >>> merge_lists([1, 3, 9], [4, 7, 11])
+    [1, 3, 4, 7, 9, 11]
+    """
+
+    results = []
+
+    while len(list1) > 0 or len(list2) > 0:
+        if list1 == []:
+            results.append(list2.pop(0))
+        elif list2 == []:
+            results.append(list1.pop(0))
+        elif list1[0] < list2[0]:
+            results.append(list1.pop(0))
+        else:
+            results.append(list2.pop(0))
+
+    return results
+
+##########ADVANCED##########
+def merge_sort(lst):
+    """
+    Given a list, returns a sorted version of that list.
+    Finish the merge sort algorithm by writing another function that
+    that takes in a single unsorted list of integers and uses recursion and the 'merge_lists'
+    function you already wrote to return a new sorted list containing all integers from
+    thin input list. In other words, the new function should sort a list using merge_lists
+    and recursion.
+    >>> merge_sort([6, 2, 3, 9, 0, 1])
+    [0, 1, 2, 3, 6, 9]
+    """
+    if len(lst) < 2:
+        return lst
+
+    middle = int(len(lst) / 2)
+
+    list1 = merge_sort(lst[:middle])
+    list2 = merge_sort(lst[middle:])
+
+    return merge_lists(list1, list2)
+
+
+
+#####################################################################
+# END OF ASSIGNMENT: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
